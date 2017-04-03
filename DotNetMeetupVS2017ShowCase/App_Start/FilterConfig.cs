584c3b9b3363bd484fb89cffaf397e5e02339908
Andreas Willich <andreas.willich@techtalk.at>
2017-04-03 21:20:53 +0200

    add solution

---
+++ DotNetMeetupVS2017ShowCase/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetMeetupVS2017ShowCase
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DotNetMeetupVS2017ShowCase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotNetMeetupVS2017ShowCase.MvcApplication" Language="C#" %>
